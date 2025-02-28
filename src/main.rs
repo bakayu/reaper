17079395c165dcedfd779f6b62b41d2d7e704ff3
bakayu <ayu.0067abc@gmail.com>
2025-02-28 17:28:05 +0530

    refactor: move logic to src/lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use reaper::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,25 +12,8 @@ fn main() {
 
     println!("Searching for {} in {}", config.query, config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Unable to read the file.");
-
-    println!("Text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = reaper::run(config) {
+        println!("error: {e}");
+        process::exit(1);
     }
 }
